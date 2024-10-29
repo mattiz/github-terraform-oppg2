cec85913413e413314ca0d6054779086fc4f2bee
Mathias Bjerke <mathbje@gmail.com>
2024-10-29 21:50:36 +0100

    Changes

--- terraform/webapp/modules/loadbalancer/variables.tf
+++ terraform/webapp/modules/loadbalancer/variables.tf
@@ -1,13 +1,11 @@
 variable "rgname" {
   type        = string
   description = "The name of the resource group"
-  default     = ""
 }
 
 variable "location" {
   type        = string
   description = "The location/region of the resources group"
-  default     = "westeurope"
 }
 
 variable "common_tags" {
--- terraform/webapp/modules/serviceplan/variables.tf
+++ terraform/webapp/modules/serviceplan/variables.tf
@@ -1,13 +1,11 @@
 variable "rgname" {
   type        = string
   description = "The name of the resource group"
-  default     = ""
 }
 
 variable "location" {
   type        = string
   description = "The location/region of the resources group"
-  default     = "westeurope"
 }
 
 variable "common_tags" {
@@ -24,11 +22,9 @@ variable "basename" {
 variable "os_type" {
   type        = string
   description = "The OS type of the Service Plan"
-  default     = "Linux"
 }
 
 variable "sku_name" {
   type        = string
   description = "The SKU name of the Service Plan"
-  default     = "P1v2"
 }
