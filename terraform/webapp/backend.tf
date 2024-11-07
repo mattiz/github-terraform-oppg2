966e12ce381a35eac09341c1e19783de351198b7
Mathias Bjerke <mathbje@gmail.com>
2024-11-07 18:28:16 +0100

    Update backend config

--- terraform/webapp/backend.tf
+++ terraform/webapp/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-mathbje-oppg2-backend"
-    storage_account_name = "mbo2bny6hg1toy6"
+    storage_account_name = "mbo2bd8oyt9t8ng"
     container_name       = "tfstate"
     key                  = "terraform-webapp.tfstate"
   }
